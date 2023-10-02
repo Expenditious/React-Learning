594be507d128bfc973e0130030261722da93ec73
Expenditious <138979096+Expenditious@users.noreply.github.com>
2023-10-02 11:03:59 +0700

    Key Props

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,16 +3,15 @@ import './Transaction.css'
 
 const Transaction = () => {
   const data = [
-    {title:"ค่ารักษาพยาบาล",amount:2000},
-    {title:"ค่าน้ำมัน",amount:5000},
-    {title:"ค่าเช่าบ้าน",amount:8000},
-    {title:"จ่ายค่าประกัน",amount:300}
+    {id:1,title:"ค่ารักษาพยาบาล",amount:2000},
+    {id:2,title:"ค่าน้ำมัน",amount:5000},
+    {id:3,title:"ค่าเช่าบ้าน",amount:8000},
+    {id:4,title:"จ่ายค่าประกัน",amount:300}
   ]
     return (
     <ul className="item-list">
-     <Item title={data[0].title} amount = {data[0].amount}/>
       {data.map((element)=>{
-          return  <Item {...element}/>
+          return  <Item {...element} key = {element.id} />
       })}
     </ul>
     );
