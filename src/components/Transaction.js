1e2ec8b02ea3c249f992bc4d97e684048a0aa206
Expenditious <138979096+Expenditious@users.noreply.github.com>
2023-10-02 11:36:51 +0700

    Props Types

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,10 +4,12 @@ import { v4 as uuidv4 } from 'uuid';
 
 const Transaction = () => {
   const data = [
-    {title:"ค่ารักษาพยาบาล",amount:2000},
-    {title:"ค่าน้ำมัน",amount:5000},
-    {title:"ค่าเช่าบ้าน",amount:8000},
-    {title:"จ่ายค่าประกัน",amount:300}
+    {title:"ค่าขนส่ง",amount:8000},
+    {title:"ค่าน้ำมัน",amount:2000},
+    {title:"ค่ากิน",amount:3000},
+    {title:"เงินเดือน",amount:30000},
+    {title:"ค่าประกัน",amount:300},
+    {title:"ค่าเดินทาง",amount:800}
   ]
     return (
     <ul className="item-list">
--- src/components/item.js
+++ src/components/item.js
@@ -1,7 +1,14 @@
+import PropTypes from 'prop-types'; // ES6
+
 const Item = ({title,amount}) => {
     return (
-    <li>{title}<span>-{amount}</span></li>
+    <li>{title}<span> {amount}</span></li>
     );
+}
+Item.propTypes={
+  title:PropTypes.string.isRequired,
+  amount:PropTypes.number.isRequired
+
 }
 
 export default Item
