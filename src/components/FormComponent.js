0313a7079a191aba6cd0c14d19da3ed2d9087418
Expenditious <138979096+Expenditious@users.noreply.github.com>
2023-10-03 11:54:28 +0700

    React Hook useState

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,26 +1,39 @@
 import './FormComponent.css'
+import { useState } from 'react'
 
 const FormComponent = () =>{
+
+
+    const [title,setTitle] = useState('')
+    const [amount,setAmount] = useState(0)
+    
+
     const inputTitle = (event) =>{
-        console.log(event.target.value)
+        setTitle(event.target.value)
     }
     const inputAmount = (event) =>{
-        console.log(event.target.value)
+        setAmount(event.target.value)
     }
     const saveItem = (event) => {
-        event.prevenDefault();
-        console.log("บันทึกข้อมูลเรียบร้อย")
+        event.preventDefault();
+        const itemData = {
+            title : title,
+            amount : Number(amount)
+        }
+        console.log(itemData);
+        setTitle('')
+        setAmount(0)
     }
     return(
         <div>
             <form onSubmit={saveItem}>
                 <div className="form-control">
                     <label>ชื่อรายการ</label>
-                    <input type="text" placeholder="ระบุชื่อรายการของคุณ" onChange={inputTitle}/>
+                    <input type="text" placeholder="ระบุชื่อรายการของคุณ" onChange={inputTitle} value={title}/>
                 </div>
                 <div className="form-control">
                     <label>จำนวนเงิน</label>
-                    <input type="number" placeholder="(+ รายรับ, - รายจ่าย)" onChange={inputAmount} />
+                    <input type="number" placeholder="(+ รายรับ, - รายจ่าย)" onChange={inputAmount} value={amount} />
                 </div>
                 <div>
                     <button type ="submit" className="btn">เพิ่มข้อมูล</button>
